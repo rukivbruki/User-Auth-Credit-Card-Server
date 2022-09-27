da471339e3fa344fdba33e98b281e79a4a7a6614
rukivbruki <0994486@gmail.com>
2022-09-28 01:36:11 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('helpers/jwt');
+const errorHandler = require('helpers/error-handler');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(jwt());
+
+app.use('/users', require('./users/users-controller'));
+
+app.use(errorHandler);
+
+const port =
+  process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
+const server = app.listen(port, function () {
+  console.log('Server listening on port ' + port);
+});
---
+++ users/user-service.js
@@ -0,0 +1,74 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('helpers/db');
+const User = db.User;
+
+module.exports = {
+  authenticate,
+  getAll,
+  getById,
+  create,
+  update,
+  delete: _delete,
+};
+
+async function authenticate({ username, password }) {
+  const user = await User.findOne({ username });
+  console.log(user);
+  if (user && bcrypt.compareSync(password, user.hash)) {
+    const token = jwt.sign({ sub: user.id }, config.secret, {
+      expiresIn: '7d',
+    });
+    return {
+      ...user.toJSON(),
+      token,
+    };
+  }
+}
+
+async function getAll() {
+  return await User.find();
+}
+
+async function getById(id) {
+  return await User.findById(id);
+}
+
+async function create(userParam) {
+  if (await User.findOne({ username: userParam.username })) {
+    throw 'Username "' + userParam.username + '" is already taken';
+  }
+
+  const user = new User(userParam);
+
+  if (userParam.password) {
+    user.hash = bcrypt.hashSync(userParam.password, 10);
+  }
+
+  await user.save();
+}
+
+async function update(id, userParam) {
+  const user = await User.findById(id);
+
+  if (!user) throw 'User not found';
+  if (
+    user.username !== userParam.username &&
+    (await User.findOne({ username: userParam.username }))
+  ) {
+    throw 'Username "' + userParam.username + '" is already taken';
+  }
+
+  if (userParam.password) {
+    userParam.hash = bcrypt.hashSync(userParam.password, 10);
+  }
+
+  Object.assign(user, userParam);
+
+  await user.save();
+}
+
+async function _delete(id) {
+  await User.findByIdAndRemove(id);
+}
